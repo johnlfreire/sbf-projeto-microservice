7e2c6e3eede3688e5981b911802fb906316bc823
johnlfreire <john-jtw@hotmail.com>
2020-11-02 19:45:08 -0300

    Security Eureka server

---
+++ eureka-server/src/main/java/com/br/sfb/eurekaserver/config/WebSecurity.java
@@ -0,0 +1,19 @@
+package com.br.sfb.eurekaserver.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().ignoringAntMatchers("/eureka/**");
+		super.configure(http);
+	}
+	public String filterType() {
+	    // TODO Auto-generated method stub
+	    return "post";
+	}
+}
